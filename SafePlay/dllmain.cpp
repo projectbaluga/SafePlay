ac535399198b9078289630fcaa07f5b61cb79401
projectbaluga <79056737+projectbaluga@users.noreply.github.com>
2025-08-23 13:34:17 +0800

    Revert "Use CreateProcess for launching RagnaPH"

--- SafePlay/dllmain.cpp
+++ SafePlay/dllmain.cpp
@@ -563,20 +563,17 @@ static DWORD WINAPI LaunchGameThread(LPVOID) {
     GetModuleFileNameW(g_hModule, base, MAX_PATH);
     PathRemoveFileSpecW(base);
 
-    wchar_t exePath[MAX_PATH];
-    swprintf(exePath, MAX_PATH, L"%s\\RagnaPH.exe", base);
+    SHELLEXECUTEINFOW sei{};
+    sei.cbSize = sizeof(sei);
+    sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+    sei.lpFile = L"RagnaPH.exe";
+    sei.nShow = SW_SHOWNORMAL;
+    sei.lpDirectory = base;
 
-    STARTUPINFOW si{};
-    si.cb = sizeof(si);
-    PROCESS_INFORMATION pi{};
-
-    if (CreateProcessW(exePath, NULL, NULL, NULL, FALSE, 0, NULL, base, &si, &pi)) {
-        WaitForSingleObject(pi.hProcess, INFINITE);
-        CloseHandle(pi.hThread);
-        CloseHandle(pi.hProcess);
+    if (ShellExecuteExW(&sei)) {
+        WaitForSingleObject(sei.hProcess, INFINITE);
+        CloseHandle(sei.hProcess);
     }
-
-    ExitProcess(0);
     return 0;
 }
 
